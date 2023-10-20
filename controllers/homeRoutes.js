9026240c9c2a38787cf0e35f50f7f9d176fb276f
Kayla <sullenspartyof3@gmail.com>
2023-10-20 09:44:34 -0700

    User model and homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Blog, User } = require('../models');
+
+router.get('/', (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: {
+                {
+                    model: User,
+                    attributes: ["name"],
+                },
+            },      
+        });
+
+        const blogs = blogData.map((project) => project.get({ plain: true }));
+
+        res.render('homepage', { projects, logged_in: req.session.logged_in });
+    } catch (error) {
+        res.status(500).json(error);   
+    }
+});
