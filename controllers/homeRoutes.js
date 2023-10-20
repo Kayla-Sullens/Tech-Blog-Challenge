35a82d892a26b526a529c5db454dd3371d63b94c
Kayla <sullenspartyof3@gmail.com>
2023-10-20 10:36:00 -0700

    Installed dependencies

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,16 +6,18 @@ router.get('/', (req, res) => {
         const blogData = await Blog.findAll({
             include: {
                 {
-                    model: User,
-                    attributes: ["name"],
-                },
-            },      
-        });
-
-        const blogs = blogData.map((project) => project.get({ plain: true }));
-
-        res.render('homepage', { projects, logged_in: req.session.logged_in });
-    } catch (error) {
-        res.status(500).json(error);   
-    }
+                model: User,
+                attributes: ["name"],
+            },
+            },
 });
+
+const blogs = blogData.map((project) => project.get({ plain: true }));
+
+res.render('homepage', { projects, logged_in: req.session.logged_in });
+    } catch (error) {
+    res.status(500).json(error);
+}
+});
+
+module.exports = router;
