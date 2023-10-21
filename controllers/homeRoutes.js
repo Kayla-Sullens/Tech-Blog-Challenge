a59ac02bca02184f2e11d4f642d74036d6dbf484
Kayla <sullenspartyof3@gmail.com>
2023-10-21 13:16:59 -0700

    Sign up started

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,23 +1,45 @@
 const router = require('express').Router();
 const { Blog, User } = require('../models');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const blogData = await Blog.findAll({
-            include: {
+            include: [
                 {
-                model: User,
-                attributes: ["name"],
-            },
-            },
-});
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
 
-const blogs = blogData.map((project) => project.get({ plain: true }));
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-res.render('homepage', { projects, logged_in: req.session.logged_in });
+    res.render('homepage', { blogs, logged_in: req.session.logged_in });
     } catch (error) {
     res.status(500).json(error);
 }
+});
+
+router.get('/vlog/:id', async (req, res) => {
+    try {
+      const blogData = await Blog.findByPk(req.params.id, {
+        include: [
+            {
+                model: User,
+                attributes: ['name'],
+            },
+        ],
+      });
+    
+      const blog = blogData.get({ plain: true });
+
+      res.render('blog', {
+        ...blog,
+        logged_in: req.session.logged_in,
+      });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 module.exports = router;
