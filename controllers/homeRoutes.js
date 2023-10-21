f600105267eb292fcfd114993757649b328e568b
Kayla <sullenspartyof3@gmail.com>
2023-10-21 13:35:43 -0700

    Login and Dashboard handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -42,4 +42,13 @@ router.get('/vlog/:id', async (req, res) => {
     }
 });
 
+router.get('/login', (req, res) => {
+    if(req.session.logged_in){
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login')
+});
+
 module.exports = router;
