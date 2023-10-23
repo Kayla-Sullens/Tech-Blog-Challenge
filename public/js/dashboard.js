f9c1be4abb22a89477e0f78c76e3bfd775f3b6ca
Kayla <sullenspartyof3@gmail.com>
2023-10-23 11:58:14 -0700

    Event handlers done for new blog and delete blog

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,13 +1,47 @@
 const elNewBlogForm = document.querySelector('.new-blog-form');
 const elBlogList = document.querySelector('.blog-list');
 
-const newBlogHandler = () => {};
+const newBlogHandler = async (event) => {
+    event.preventDefault();
 
-const deleteButtonHandler = () => {};
+    const title = document.querySelector('#blog-title').value.trim();
+    const date_created = document.querySelector('#blog-date').value.trim();
+    const contents = document.querySelector('#blog-content').value.trim();
+
+    if (title && date_created && contents) {
+        const response = await fetch('/api/blogs', {
+            method: 'POST',
+            body: JSON.stringify({ title, date_created, contents }),
+            headers: { 'Content-Type': 'application.json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create blog.');
+        }
+    }
+};
+
+const deleteButtonHandler = async (event) => { 
+    if(event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/blogs/${id}`, {
+            method: 'DELETE',
+        });
+
+        if(response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete blog');
+        }
+    }
+};
 
 elNewBlogForm.addEventListener('submit', newBlogHandler);
 
-if(elBlogList) {
+if (elBlogList) {
     document.addEventListener('click', deleteButtonHandler);
 }
- 
+
