844e7a45ceb521cfdc684e66787014539d940dbe
Kayla <sullenspartyof3@gmail.com>
2023-10-23 18:32:24 -0700

    Working login and signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,27 +1,24 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({
-                username,
-                email,
-                password,
-            }),
-            headers: {'Content-Type': 'application/json'}
+const signupFormHandler = async (event) => {
+      event.preventDefault();
+      const username = document.querySelector("#username-signup").value.trim();
+      const email = document.querySelector("#email-signup").value.trim();
+      const password = document.querySelector("#password-signup").value.trim();
+    
+      if (username && email && password) {
+        const response = await fetch("/api/users", {
+          method: "POST",
+          body: JSON.stringify({ username, email, password }),
+          headers: { "Content-Type": "application/json" },
         });
+    
         if (response.ok) {
-            alert('Account created! Logging you in now.');
-            document.location.replace('/dashboard');
+          document.location.replace("/");
         } else {
-            alert(response.statusText)
+          alert(response.statusText);
         }
-    }
-}
+      }
+    };
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+    document
+    .querySelector(".signup-form")
+    .addEventListener("submit", signupFormHandler);
